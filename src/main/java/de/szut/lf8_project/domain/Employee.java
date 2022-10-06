d5ab405732e09f0a4b52487b2f552738822088eb
Lars Lamperski <lars.lamperski@neuland-bfi.de>
2022-10-06 23:11:28 +0200

    MS-74 Employee-Serialisierung, Hilfsmethode in Tests die sich bei Keycloak einloggt und Tests für EmployeeService

--- src/main/java/de/szut/lf8_project/domain/Employee.java
+++ src/main/java/de/szut/lf8_project/domain/Employee.java
@@ -1,16 +1,18 @@
 package de.szut.lf8_project.domain;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 import java.util.List;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public record Employee (
-        EmployeeId id,
-        LastName lastName,
-        FirstName firstName,
-        Street street,
-        Postcode postcode,
-        List<Qualification> skillset
+        @JsonProperty("id") EmployeeId id,
+        @JsonProperty("lastName") LastName lastName,
+        @JsonProperty("firstName") FirstName firstName,
+        @JsonProperty("street") Street street,
+        @JsonProperty("postcode") Postcode postcode,
+        @JsonProperty("skillset") List<Qualification> skillset
 ){}
--- src/test/java/de/szut/lf8_project/repository/TestEmployeeRestRepository.java
+++ src/test/java/de/szut/lf8_project/repository/TestEmployeeRestRepository.java
@@ -1,14 +1,29 @@
 package de.szut.lf8_project.repository;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import de.szut.lf8_project.common.JWT;
 import de.szut.lf8_project.common.Statuscode;
+import de.szut.lf8_project.domain.Employee;
 import de.szut.lf8_project.domain.EmployeeId;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestTemplate;
 
+import java.util.Map;
+import java.util.Objects;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.springframework.test.annotation.DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD;
@@ -17,17 +32,61 @@ import static org.springframework.test.annotation.DirtiesContext.ClassMode.AFTER
 @DirtiesContext(classMode = AFTER_EACH_TEST_METHOD)
 public class TestEmployeeRestRepository {
 
+    private static boolean setUpIsDone = false;
     @Autowired
     private EmployeeRestRepository employeeRestRepository;
+    @Autowired
+    private Environment env;
+    private JWT jwt;
+
+    @BeforeEach
+    public void setUpJwt() throws JsonProcessingException {
+        if (setUpIsDone) return;
+        this.jwt = getFreshJwt();
+        setUpIsDone = true;
+    }
+
+    @Test
+    @DisplayName("Throws a exception with an 401 Statuscode if the jwt is invalid")
+    public void unauthorized() {
+        JWT jwt = new JWT("invalid");
+
+        RepositoryException exception = assertThrows(RepositoryException.class, () -> employeeRestRepository.getEmployeeById(jwt, new EmployeeId(1L)));
+
+        assertEquals(Statuscode.UNAUTHORIZED, exception.getStatuscode());
+    }
 
     @Test
     @DisplayName("Gets a Employee by ID")
     public void getEmployee() throws RepositoryException {
-        String jwt = "invalid";
+        //TODO - umbauen sobald wir Kontrolle über den Service haben oder Insert-Hilfsmethode schreiben
+        EmployeeId employeeId = new EmployeeId(115L);
 
-        RepositoryException exception = assertThrows(RepositoryException.class, () -> employeeRestRepository.getEmployeeById(new JWT(""), new EmployeeId(1L)));
+        Employee employee = employeeRestRepository.getEmployeeById(jwt, employeeId);
 
-        assertEquals(Statuscode.UNAUTHORIZED, exception.getStatuscode());
+        assertThat(employee.id()).isEqualTo(employeeId);
+    }
+
+    private JWT getFreshJwt() throws JsonProcessingException {
+        String jsonString = new RestTemplate()
+                .postForEntity(
+                        Objects.requireNonNull(env.getProperty("authProvider.url")),
+                        getPostRequestBody(),
+                        String.class)
+                .getBody();
+        Map<String, String> map = new ObjectMapper().readValue(jsonString, Map.class);
+        return new JWT("Bearer " + map.get("access_token"));
+    }
+
+    private HttpEntity<MultiValueMap<String, String>> getPostRequestBody() {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+        MultiValueMap<String, String> bodyParamMap = new LinkedMultiValueMap<>();
+        bodyParamMap.add("grant_type", "password");
+        bodyParamMap.add("client_id", env.getProperty("authProvider.client_id"));
+        bodyParamMap.add("username", env.getProperty("authProvider.user"));
+        bodyParamMap.add("password", env.getProperty("authProvider.password"));
+        return new HttpEntity<>(bodyParamMap, headers);
     }
 
 }
