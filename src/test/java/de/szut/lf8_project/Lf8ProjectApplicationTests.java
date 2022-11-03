0dbcabf3f396d483c69f5f5413c5edfce20ad461
Johann Weise <github@jcpw.de>
2022-11-03 18:21:00 +0100

    SpringBootTest ohne DB ausschalten

--- src/test/java/de/szut/lf8_project/Lf8ProjectApplicationTests.java
+++ src/test/java/de/szut/lf8_project/Lf8ProjectApplicationTests.java
@@ -1,5 +1,7 @@
 package de.szut.lf8_project;
 
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -7,6 +9,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 class Lf8ProjectApplicationTests {
 
     @Test
+    @Disabled
+    // Dieser Test funktioniert (natürlich) nicht in der Pipeline, weil er eine lokale DB vorraussetzt
     void contextLoads() {
     }
 
