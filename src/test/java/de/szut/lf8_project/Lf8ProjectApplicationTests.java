0b43da191ed7c34ac6f927cd3e3a5730f14d3bee
Johann Weise <github@jcpw.de>
2022-11-03 18:26:47 +0100

    Test anders ausschalten?

--- src/test/java/de/szut/lf8_project/Lf8ProjectApplicationTests.java
+++ src/test/java/de/szut/lf8_project/Lf8ProjectApplicationTests.java
@@ -5,11 +5,11 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@Disabled
 @SpringBootTest
 class Lf8ProjectApplicationTests {
 
     @Test
-    @Disabled
     // Dieser Test funktioniert (natürlich) nicht in der Pipeline, weil er eine lokale DB vorraussetzt
     void contextLoads() {
     }
