cdf76e8ee3054ea39a27727f536aa72ee78870a3
Johann Weise <github@jcpw.de>
2022-11-04 09:30:03 +0100

    Unused import

--- src/test/java/de/szut/lf8_project/Lf8ProjectApplicationTests.java
+++ src/test/java/de/szut/lf8_project/Lf8ProjectApplicationTests.java
@@ -1,7 +1,6 @@
 package de.szut.lf8_project;
 
 import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
