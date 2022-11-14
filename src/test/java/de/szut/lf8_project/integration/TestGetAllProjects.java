3733c5a11e3b0249d5863c22875003cf4899d8da
Johannes Lohmer <lohmer@univention.de>
2022-11-14 18:32:46 +0100

    MS-12 fix/improve getAllProjects integration Tests

--- src/test/java/de/szut/lf8_project/integration/TestGetAllProjects.java
+++ src/test/java/de/szut/lf8_project/integration/TestGetAllProjects.java
@@ -1,13 +1,19 @@
 package de.szut.lf8_project.integration;
 
 import de.szut.lf8_project.FullIntegrationTest;
+import de.szut.lf8_project.domain.employee.Employee;
+import de.szut.lf8_project.domain.employee.ProjectRole;
 import de.szut.lf8_project.domain.project.Project;
+import de.szut.lf8_project.domain.project.TeamMember;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.ResultActions;
 
+import java.util.List;
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @DisplayName("Die GET All Projects Rest-Methode")
@@ -16,7 +22,14 @@ public class TestGetAllProjects extends FullIntegrationTest {
     @Test
     @DisplayName("sollte alle Projekte anzeigen")
     void getAllProjects() throws Exception {
-        // Project project = createProjectInDatabase();
+        Project project1 = createAndSaveDefaultProjectWithProjectLead();
+
+        Project tmp_project = createAndSaveDefaultProjectWithProjectLead();
+
+        ProjectRole projectRole = createAndSaveQualificationInRemoteRepository();
+        Employee employee = saveEmployeeInRemoteRepository(createDefaultEmployeeWithRolesWith0Id(List.of(projectRole)));
+        tmp_project.getTeamMembers().add(new TeamMember(employee.getId(), projectRole));
+        Project project2 = saveProjectInDatabase(tmp_project);
 
         ResultActions result = mockMvc.perform(get("/api/v1/project")
                 .contentType(MediaType.APPLICATION_JSON_VALUE)
@@ -24,7 +37,16 @@ public class TestGetAllProjects extends FullIntegrationTest {
         );
 
         result
-                .andExpect(status().isOk());
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$").isArray())
+                .andExpect(jsonPath("$[0]").isNotEmpty())
+                .andExpect(jsonPath("$[1]").isNotEmpty())
+                .andExpect(jsonPath("$[0].projectId").isNotEmpty())
+                .andExpect(jsonPath("$[1].projectId").isNotEmpty())
+                .andExpect(jsonPath("$[0].projectName").value(project2.getProjectName().unbox()))
+                .andExpect(jsonPath("$[1].projectName").value(project2.getProjectName().unbox()))
+                .andExpect(jsonPath("$[0].teamMember").isEmpty())
+                .andExpect(jsonPath("$[1].teamMember").isNotEmpty());
     }
 
     @Test
@@ -34,6 +56,9 @@ public class TestGetAllProjects extends FullIntegrationTest {
                 .contentType(MediaType.APPLICATION_JSON_VALUE)
         );
 
-        result.andExpect(status().is(401));
+        result
+                .andExpect(status().is(401))
+                .andExpect(jsonPath("$[0]").doesNotExist())
+                .andExpect(jsonPath("$[0].projectId").doesNotExist());
     }
 }
